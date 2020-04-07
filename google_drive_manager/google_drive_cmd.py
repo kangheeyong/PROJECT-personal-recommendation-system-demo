654903b2c794889a376226ce31f07e344f12ea8a
kang hee yong <cagojeiger@naver.com>
2020-04-08 00:07:20 +0900

    google drive manager proto v0.0.1

--- google_drive_manager/google_drive_cmd.py
+++ google_drive_manager/google_drive_cmd.py
@@ -1,27 +1,46 @@
 import cmd
 
+import json
 from fire import Fire
 from websocket import create_connection
 
+from Feynman.algorithms.tree.print_tree import print_tree
+
 
 class template_cmd(cmd.Cmd):
-    intro = 'This is templete cmd'
-    prompt = 'templete_cmd > '
+    intro = '''
+this is google drive manager
+version 0.0.1
+            '''
+    prompt = 'google_drive > '
     file = None
 
-    def do_send(self, arg):
-        if not arg:
-            return
-        self._send(arg)
+    def do_all(self, arg):
+        all_dic = self._send('all_dic', arg)
+        data_dic = self._send('data_dic', arg)
+        adj_dic = self._send('adj_dic', arg)
+        for key in all_dic.keys():
+            print_tree(key, adj_dic)
 
-    def _send(self, name):
-        uri = "ws://localhost:8765/123"
+    def do_ls(self, arg):
+        root = self._send('root', arg)
+        data_dic = self._send('data_dic', arg)
+        adj_dic = self._send('adj_dic', arg)
+        print_tree(root, adj_dic)
+
+    def do_upload(self, arg):
+        print('not yet')
+
+    def do_download(self, arg):
+        print('not yet')
+
+    def _send(self, cmd, arg):
+        uri = 'ws://localhost:8765/'+cmd
         ws = create_connection(uri)
-        ws.send(name)
-        print('> {}'.format(name))
-        greeting = ws.recv()
-        print('< {}'.format(greeting))
+        ws.send(arg)
+        result = ws.recv()
         ws.close()
+        return json.loads(result)
 
     def run(self):
         self.cmdloop()
--- google_drive_manager/google_drive_manager.py
+++ google_drive_manager/google_drive_manager.py
@@ -2,38 +2,64 @@ import time
 import asyncio
 import websockets
 
+import json
 from fire import Fire
 
 from Feynman.etc.util import get_logger
+from Feynman.cloud import Google_drive_data_base
 
 
 class template_manager():
     def __init__(self):
         self.logger = get_logger()
 
+    def _check(self):
+        import pickle
+        with open('google_drive_data_example.pickle', 'rb') as f:
+            data = pickle.load(f)
+        self.file_data = Google_drive_data_base(data)
+        self.file_data.pruning_overlap_file()
+        self.file_data.pruning_zombie_file()
+
     async def _task(self):
         self.logger.info('Start task...')
         while True:
             begin_t = time.time()
 
-            print('tesk test')
+            self._check()
 
             sleep_t = max(0, 60 - int(time.time() - begin_t))
             self.logger.info('Sleep {} secs before next start'.format(sleep_t))
             await asyncio.sleep(sleep_t)
 
+    def do_remove_set(self, arg):
+        return self.file_data.remove_set
+
+    def do_all_dic(self, arg):
+        return self.file_data.all_dic
+
+    def do_view_dic(self, arg):
+        return self.file_data.view_dic
+
+    def do_adj_dic(self, arg):
+        return self.file_data.adj_dic
+
+    def do_data_dic(self, arg):
+        return self.file_data.data_dic
+
+    def do_root(self, arg):
+        return self.file_data.root
+
     async def _cmd_recv(self, ws, path):
-        self.logger.info('Start consumer... at {}:{}{}'.format(ws.host, ws.port, path))
-        name = await ws.recv()
-        print('< {}'.format(name))
-        greeting = 'Hello {}!'.format(name)
-        await ws.send(greeting)
-        print('> {}'.format(greeting))
+        arg = await ws.recv()
+        self.logger.info('Start consumer... at {}:{}{} {}'.format(ws.host, ws.port, path, arg))
+        func = getattr(self, 'do_' + path[1:])
+        result = func(arg)
+        await ws.send(json.dumps(result))
 
     async def _main(self):
-        coroutine_list = []
-        coroutine_list.append(websockets.serve(self._cmd_recv, '0.0.0.0', 8765))
-        coroutine_list.append(self._task())
+        coroutine_list = [websockets.serve(self._cmd_recv, '0.0.0.0', 8765),
+                          self._task()]
         await asyncio.gather(*coroutine_list)
 
     def run(self):
